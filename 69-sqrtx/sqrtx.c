cba34e48606f11b834577a975612121963631a57
Vijesh <vijeshwins.2008@gmail.com>
2026-08-31 18:57:02 +0530

    Time: 22 ms (6.66%) | Memory: 9.4 MB (11.22%) - LeetSync

--- 69-sqrtx/sqrtx.c
+++ 69-sqrtx/sqrtx.c
@@ -1,5 +1,5 @@
 int mySqrt(int x) {
-    for(long long int i=0;i<50000;i++){
+    for(long long int i=0;i<=(x/2+1);i++){
         if(i*i==x||(i*i<x&&(i+1)*(i+1)>x)){
             return i;
             break;
