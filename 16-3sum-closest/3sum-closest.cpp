940aa573af016551c45940c8830ca0968a3340a0
Vijesh <vijeshwins.2008@gmail.com>
2026-09-04 23:28:47 +0530

    Time: 603 ms (5.05%) | Memory: 14 MB (50.48%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -1,67 +1,25 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int i, j, k, l = 0;
-        long long int s = 1000000000;
+        long long s = 1000000000;
+        int l = 0;
 
-        if(target < 0) {
+        for(int i = 0; i < nums.size() - 2; i++) {
+            for(int j = i + 1; j < nums.size() - 1; j++) {
+                for(int k = j + 1; k < nums.size(); k++) {
 
-            for(i = 0; i < nums.size() - 2; i++) {
-                for(j = i + 1; j < nums.size() - 1; j++) {
-                    for(k = j + 1; k < nums.size(); k++) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[k];
 
-                        long long int sum = (long long)nums[i] + nums[j] + nums[k];
+                    if(sum == target)
+                        return target;
 
-                        if(sum == target) {
-                            return target;
-                        }
-                        else if(sum > target) {
-                            if(abs(sum - target) < abs(s)) {
-                                s = sum - target;
-                                l = s;
-                            }
-                        }
-                        else {
-                            if(abs(target - sum) < abs(s)) {
-                                s = target - sum;
-                                l = -s;
-                            }
-                        }
+                    if(abs(sum - target) < abs((long long)l - target) || l == 0) {
+                        l = sum;
                     }
                 }
             }
-
-            return target + l;
         }
 
-        else {
-
-            for(i = 0; i < nums.size() - 2; i++) {
-                for(j = i + 1; j < nums.size() - 1; j++) {
-                    for(k = j + 1; k < nums.size(); k++) {
-
-                        long long int sum = (long long)nums[i] + nums[j] + nums[k];
-
-                        if(sum == target) {
-                            return target;
-                        }
-                        else if(sum < target) {
-                            if(abs(sum - target) < abs(s)) {
-                                s = abs(sum - target);
-                                l = target - s;
-                            }
-                        }
-                        else {
-                            if(abs(sum - target) < abs(s)) {
-                                s = abs(sum - target);
-                                l = target + s;
-                            }
-                        }
-                    }
-                }
-            }
-
-            return l;
-        }
+        return l;
     }
 };
